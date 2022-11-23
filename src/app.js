69cae09455a996c9d378b46453ee1bdb4dcd43d9
kirbygorospe <kirbygorospe051987@gmail.com>
2022-11-23 01:47:19 +0100

    Added relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -28,14 +28,22 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#windSpeed");
   let dateElement = document.querySelector("#date");
+  let iconeElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconeElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconeElement.setAttribute("alt", response.data.condition.description);
 }
 let apiKey = "36a6tbf64b5d8200e48c130fc3a0o159";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={Milan}&key=${apiKey}`;
+let city = "Milan";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
 axios.get(apiUrl).then(displayTemperature);
