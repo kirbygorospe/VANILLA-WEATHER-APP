3330dbfd31ce8617a7dda4732024948a99ec8472
kirbygorospe <kirbygorospe051987@gmail.com>
2022-11-27 04:54:57 +0100

    Injected HTML weather forecast section to JS

--- src/app.js
+++ src/app.js
@@ -21,6 +21,35 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+       <div class="col-2">
+         <div class="weather-forecast-date">${day}</div>
+         <img
+           src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Weather-sun-clouds-hard-shower.svg/2048px-Weather-sun-clouds-hard-shower.svg.png"
+                  alt=""
+                />
+                <div class="weather-forecast-temperatures">
+           <span class="weather-forecast-temperature-max"> 18° </span>
+           <span class="weather-forecast-temperature-min"> 12° </span>
+         </div>
+       </div>
+   `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -86,3 +115,4 @@ let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelciusTemperature);
 
 search("Milan");
+displayForecast();
